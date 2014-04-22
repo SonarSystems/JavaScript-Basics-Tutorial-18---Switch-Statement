f5463558ef1f2164374f810cb52a53372962f846
Sonar Systems <admin@sonarsystems.co.uk>
2014-04-22 16:00:48 +0100

    JavaScript Switch Statement Introduction

---
+++ main.js
@@ -0,0 +1,23 @@
+var x = -1;
+
+switch(x)
+{
+	case -1:
+	case -2:
+	case 0:
+		alert("0");
+		break;
+
+	case 1:
+		alert("1");
+		break;
+
+	case 2:
+		alert("2");
+		break;
+
+	default:
+		alert("DEFAULT");
+}
+
+//alert("Hello World!");
